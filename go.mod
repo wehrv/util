f98fef0d7e4cf8771d1182ab30c5cc7138536570
billy harvey <billy.harvey@gmail.com>
2022-11-14 15:53:47 -0500

    .

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wehrv/util
+
+go 1.19
+
+require golang.org/x/text v0.4.0
---
+++ util.go
@@ -0,0 +1,81 @@
+package util // github.com/wehrv/util
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func isErr(err error) bool {
+	return err != nil
+}
+
+func noErr(err error) bool {
+	return err == nil
+}
+
+func abErr(err error) {
+	if isErr(err) {
+		log.Fatal(err)
+	}
+}
+
+func ppErr(err error) bool {
+	if isErr(err) {
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func npErr(err error) bool {
+	return !ppErr(err)
+}
+
+func ipErr(err error) bool {
+	return ppErr(err)
+}
+
+func isEql(a, b, c, d string) string {
+	if a == b {
+		return c
+	}
+	return d
+}
+
+func fetch(url string) ([]byte, error) {
+	val, err := http.Get(url)
+	if ipErr(err) {
+		return []byte{}, err
+	}
+	defer val.Body.Close()
+	return io.ReadAll(val.Body)
+}
+
+func ungz(data []byte) ([]byte, error) {
+	datb, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return data, err
+	}
+	return io.ReadAll(datb)
+}
+
+func snakeToCamel(str string) string {
+	//	fmt.Println(str)
+	fields := strings.Fields(strings.TrimSpace(strings.Replace(str, "_", " ", -1)))
+	//	fmt.Println(fields)
+	for field := range fields {
+		caser := cases.Title(language.AmericanEnglish)
+		fields[field] = caser.String(fields[field])
+	}
+	//	fmt.Println(fields)
+	header := strings.Join(fields, "")
+	//	fmt.Println(header)
+	return header
+}
