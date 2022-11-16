35f842a94e55284ea8f1ab2c2b390802b02082fe
billy harvey <billy.harvey@gmail.com>
2022-11-16 15:07:34 -0500

    .

--- body.go
+++ body.go
@@ -11,19 +11,19 @@ import (
 )
 
 type Body struct {
-	Body  []byte
-	Error error
-	Case  string
-	Mime  string
-	Path  []string
-	Maps  map[string]string
-	R     *http.Request
-	W     http.ResponseWriter
+	Body   []byte
+	Error  error
+	Case   string
+	Mime   string
+	Path   []string
+	Maps   map[string]string
+	Reader *http.Request
+	Writer http.ResponseWriter
 }
 
 func (body Body) New(w http.ResponseWriter, r *http.Request) *Body {
-	body.R = r
-	body.W = w
+	body.Reader = r
+	body.Writer = w
 	body.Path = strings.Split(r.URL.Path[1:], "/")
 	body.Maps = make(map[string]string)
 	body.Case = body.Path[0]
@@ -71,19 +71,18 @@ func (body *Body) File(file string) *Body {
 }
 
 func (body *Body) Send() *Body {
-	dots := strings.Split(body.Path[len(body.Path)-1], ".")
-	body.Mime = dots[len(dots)-1]
-	if body.Mime == "" {
-		body.Mime = "text"
-	}
-	body.Mime = mime.TypeByExtension("." + body.Mime)
-	if body.Mime == "application/json" {
-		body.Mime += ";charset=utf-8"
-	}
-	log.Println(body.Path, body.Mime, len(body.Body))
 	if body.Error == nil {
-		body.W.Header().Set("Content-Type", body.Mime)
-		_, body.Error = body.W.Write(body.Body)
+		dots := strings.Split(body.Path[len(body.Path)-1], ".")
+		body.Mime = dots[len(dots)-1]
+		if body.Mime == "" {
+			body.Mime = "text"
+		}
+		body.Mime = mime.TypeByExtension("." + body.Mime)
+		if body.Mime == "application/json" {
+			body.Mime += "; charset=utf-8"
+		}
+		body.Writer.Header().Set("Content-Type", body.Mime)
+		_, body.Error = body.Writer.Write(body.Body)
 	}
 	return body
 }
